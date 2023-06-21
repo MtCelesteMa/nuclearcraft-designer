747d77576f3dc6b58163db98bbfc5c8df44ea0c2
Celeste Ma <gerry.y.ma@gmail.com>
2023-06-21 21:55:12 +0800

    Bumped version to v0.0.1. Reorganized code for rotor blade design.

---
+++ src/nuclearcraft_designer/overhauled/turbine_rotor_blade/data_structures.py
@@ -0,0 +1,23 @@
+"""NuclearCraft: Overhauled turbine rotor blades."""
+
+
+class RotorBlade:
+    """An object representing a NuclearCraft: Overhauled turbine rotor blade."""
+    def __init__(self, name: str, efficiency: float, expansion: float) -> None:
+        """Constructs a RotorBlade object.
+
+        :param name: The name of the rotor blade.
+        :param efficiency: The efficiency of the rotor blade.
+        :param expansion: The expansion of the rotor blade.
+        """
+        self.name = name
+        self.efficiency = efficiency
+        self.expansion = expansion
+
+
+STEEL = RotorBlade("steel", 1.0, 1.4)
+EXTREME = RotorBlade("extreme", 1.1, 1.6)
+SIC_SIC_CMC = RotorBlade("sic_sic_cmc", 1.2, 1.8)
+STATOR = RotorBlade("stator", -1.0, 0.75)
+
+ROTOR_BLADE_TYPES = [STEEL, EXTREME, SIC_SIC_CMC, STATOR]
